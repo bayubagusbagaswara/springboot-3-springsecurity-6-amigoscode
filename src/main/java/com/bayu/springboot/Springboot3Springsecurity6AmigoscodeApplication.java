c940ec8f25a5051c1d8df0635e210e7ce66a6d7d
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-01-11 08:47:11 +0700

    initial commit

---
+++ src/main/java/com/bayu/springboot/Springboot3Springsecurity6AmigoscodeApplication.java
@@ -0,0 +1,13 @@
+package com.bayu.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot3Springsecurity6AmigoscodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot3Springsecurity6AmigoscodeApplication.class, args);
+	}
+
+}
